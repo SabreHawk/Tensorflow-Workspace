4f7a4f247e1926e1f8e16d7249f9c96c9088d49f
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-01-18 19:41:43 +0800

    Upload Recognize-Flower Data 2018/01/18 - 19:41

---
+++ BP-Recognize-Flower/bp-recognize-flower.py
@@ -0,0 +1,28 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+sess = tf.InteractiveSession()
+
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
+
+w = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+
+y = tf.nn.softmax(tf.matmul(x,w)+b)
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(200)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print (sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
